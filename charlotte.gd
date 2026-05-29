16d6f6b6a05a74c74931e782e888bb5b1aa0823e
RokkuMannSlayer <dante.mounes@gmail.com>
2026-05-29 18:15:30 -0300

    2
    1) Zoom de la cámara alejada
    2) Piso más expandido
    3) Sprint añadido

--- charlotte.gd
+++ charlotte.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var speed: float = 350.0
+@export var walk_speed: float = 350.0
+@export var run_speed: float = 600.0
 
 func _physics_process(_delta: float) -> void:
 	var direction = Vector2.ZERO
@@ -13,9 +14,14 @@ func _physics_process(_delta: float) -> void:
 		direction.y -= 1
 	if Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN):
 		direction.y += 1
+	
+	var current_speed = walk_speed
+	
+	if Input.is_key_pressed(KEY_SHIFT):
+		current_speed = run_speed
 
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
 
-	velocity = direction * speed
+	velocity = direction * current_speed
 	move_and_slide()
