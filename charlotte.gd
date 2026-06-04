68df0774fccc007cd5b9b040279857e3c96d5742
RokkuMannSlayer <dante.mounes@gmail.com>
2026-06-04 19:01:03 -0300

    Movimientos actualizados
    al caminar: pierde 1 punto cada 5 segundos
    al correr: pierde 1 punto cada 2,5 segundos

--- charlotte.gd
+++ charlotte.gd
@@ -4,9 +4,11 @@ extends CharacterBody2D
 @export var run_speed: float = 600.0
 
 @export var maxHealth: int = 100
-var currentHealth: float
+var currentHealth: int
 
-@export var healthDrainRate: float = 1.0
+# Tiempo necesario para perder 1 punto de salud
+@export var walk_health_interval: float = 5.0
+@export var run_health_interval: float = 2.5
 
 var healthTimer: float = 0.0
 
@@ -18,7 +20,7 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.ZERO
-
+	
 	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
 		direction.x -= 1
 	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
@@ -27,28 +29,37 @@ func _physics_process(delta: float) -> void:
 		direction.y -= 1
 	if Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN):
 		direction.y += 1
-
+	
 	var current_speed = walk_speed
-
+	var is_running = false
+	
 	if Input.is_key_pressed(KEY_SHIFT):
 		current_speed = run_speed
-
+		is_running = true
+	
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
-
+	
 	velocity = direction * current_speed
 	move_and_slide()
-
-	# Sistema de desgaste de salud
-	healthTimer += delta
-
-	if healthTimer >= 1.0:
-		healthTimer = 0.0
-		currentHealth -= healthDrainRate
-		healthChange.emit()
-
-	if currentHealth <= 0:
-		die()
+	
+	if direction != Vector2.ZERO:
+		healthTimer += delta
+		
+		var interval = walk_health_interval
+		
+		if is_running:
+			interval = run_health_interval
+			
+		if healthTimer >= interval:
+			healthTimer = 0.0
+			currentHealth -= 1
+			healthChange.emit()
+			
+			print("Salud:", currentHealth)
+			
+			if currentHealth <= 0:
+				die()
 
 func die():
 	print("Charlotte descendió a la locura...")
