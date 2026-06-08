3eadd3e550243a49b7dc9de3080332f0d303cd96
RokkuMannSlayer <dante.mounes@gmail.com>
2026-06-08 14:38:48 -0300

    caramelo creado (recupera 10 puntos de vida)

---
+++ drug.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var healAmount: int = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		body.currentHealth = min (body.currentHealth + healAmount, body.maxHealth)
+	#body.healthChange.emit()
+	
+	print("Recovered health: ", healAmount)
+	print("Current health: ", body.currentHealth)
+	
+	queue_free()
