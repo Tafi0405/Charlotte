71d9e6821c657928472b65f7dca627c6e45179ac
DonBrunoMT <brunomaranztracz@gmail.com>
2026-09-09 16:39:55 -0300

    Menu del inicio
    1) cree un menu de inicio al iniciar del juego
    2) cuando pprecionas el boton de jugar te pasa en una pantalla de azul

---
+++ menu_principal.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_bjugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://blue.tscn")
+
+func _on_bsalir_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().quit()
+
+
+func _on_b_configurar_pressed() -> void:
+	pass # Replace with function body.
