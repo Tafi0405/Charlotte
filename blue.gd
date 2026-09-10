5684875f460e1bca1138c18920421e4c0c3ac434
DonBrunoMT <brunomaranztracz@gmail.com>
2026-09-10 10:10:30 -0300

    Simulador de selección de niveles
    Este seria la pantalla de selección de niveles y te puede llevar del primer mundo o el segundo

---
+++ blue.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_BNivel2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/pafu.tscn")
+
+
+func _on_BNivel1_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/piso.tscn")
