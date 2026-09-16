c811099ead600f001bd835484e41a18b7420d024
DonBrunoMT <brunomaranztracz@gmail.com>
2026-09-16 17:44:54 -0300

    Atras
    Solamente agrege un boton (en la seleccion de niveles) que retrosede al menu principal o el titulo como lo llamen

--- blue.gd
+++ blue.gd
@@ -8,3 +8,7 @@ func _on_BNivel2_pressed() -> void:
 
 func _on_BNivel1_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/piso.tscn")
+
+
+func _on_b_atras_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu_principal.tscn")
